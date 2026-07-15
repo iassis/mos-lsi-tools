98fc2db0f0f6250ebd32367306ff0a1441ea49b6
iassis <igor.assis@outlook.com>
2026-07-15 16:31:53 -0600

    Update version and icon in plugin configuration

--- page/plugin.config.js
+++ page/plugin.config.js
@@ -2,8 +2,8 @@ export default {
   name: 'mos-lsi-tools',
   displayName: 'MOS LSI Tools',
   description: 'LSI tools for managing HBAs and expanders',
-  version: '0.0.0',
-  icon: '',
+  version: '1.0.3',
+  icon: 'https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/broadcom.svg',
   author: 'morgoth',
   homepage: 'https://broadcom.com',
 };
