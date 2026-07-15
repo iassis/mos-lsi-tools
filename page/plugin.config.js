fff64bdb7902c7ddcc086457cc2fdddb9d76e753
iassis <igor.assis@outlook.com>
2026-07-15 15:57:51 -0600

    Create plugin.config.js for MOS LSI Tools
    Initial configuration for MOS LSI Tools plugin.

---
+++ page/plugin.config.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'mos-lsi-tools',
+  displayName: 'MOS LSI Tools',
+  description: 'LSI tools for managing HBAs and expanders',
+  version: '0.0.0',
+  icon: '',
+  author: 'morgoth',
+  homepage: 'https://broadcom.com',
+};
